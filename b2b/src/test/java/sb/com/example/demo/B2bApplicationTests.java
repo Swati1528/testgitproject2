ad74762b05983c14d0092a8f33a7678aa2cef81f
swati <sbsoftware96@gmail.com>
2024-03-12 22:05:19 +0530

    commit

---
+++ b2b/src/test/java/sb/com/example/demo/B2bApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
